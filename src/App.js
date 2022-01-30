85f0b5e6f6cd0997d9185feb0a44579f0e0a7c7e
Susanna Yossef <susannayossef98@gmail.com>
2022-01-30 22:22:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button/Button.js';
+import Container from './components/Container/Container.js';
+import Timer from './components/Timer/Timer.js';
 
-function App() {
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState('null');
+
+  const [active, setActive] = useState(false);
+
+  const start = (e) => {
+    e.preventDefault();
+    console.log('start clicked');
+    setActive(true);
+    setTimer(
+      setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1)
+    );
+  };
+
+  const stop = (e) => {
+    e.preventDefault();
+    console.log('stop clicked');
+    if (timer) clearInterval(timer);
+    setActive(false);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setActive(false);
+    //setTimer('null');
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Timer value={time} />
+      <Button onClick={start} isInactive={active}>
+        start
+      </Button>
+      <Button onClick={stop} isInactive={!active}>
+        stop
+      </Button>
+      <Button onClick={reset} isInactive={active}>
+        reset
+      </Button>
+    </Container>
   );
-}
+};
 
 export default App;
